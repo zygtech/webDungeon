eb15e8168ccf6ec5146e18fa169c69bc49d2ebe1
ZygTech.pl <krzysztof@zygtech.pl>
2021-08-31 06:30:48 +0200

    Add files via upload

--- webDungeon.js
+++ webDungeon.js
@@ -216,7 +216,7 @@
 					else {
 						console.log(object);
 						ctx.drawImage(IMGplayer,((object[2][0]-64)/(object[2][2]*DepthAttribute)+canvas.width/2),((object[2][1]-128)/(object[2][2]*DepthAttribute)+canvas.height/2),128/(object[2][2]*DepthAttribute),256/(object[2][2]*DepthAttribute));
-						if (playern=="" && object[2][2]<3200) {
+						if (playern=="" && object[2][2]<space*3) {
 							document.getElementById("message").innerHTML="YOU HEAR: " + object[5];
 							playern=object[5];
 						}
